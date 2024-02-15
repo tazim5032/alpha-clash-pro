5aca316e0dd8425ddfcf548f73e3d4452ea0bb38
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-02-15 11:23:22 +0600

    chk alphabet added

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -6,6 +6,25 @@
     const playgroundSection = document.getElementById('play-ground');
     playgroundSection.classList.remove('hidden');
 }*/
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    //console.log('player pressed', playerPressed);
+
+    //expected pressed
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+    console.log(playerPressed, expectedAlphabet);
+
+    //chk match or not
+    if(playerPressed == expectedAlphabet){
+        console.log('got point');
+    }
+    else{
+        console.log('lost a life');
+    }
+}
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
 function continueGame(){
     //generate a random alphabet
     const alphabet = getARandomAlphabet();
